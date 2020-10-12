ccc5ed6d5c4443cde4463d0a2cd820ba4e9837cb
SriramTummala <sriramtummala87@gmail.com>
2020-10-12 14:50:24 -0500

    component changes

--- src/src/app/app.component.ts
+++ src/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { User } from './user';
+import {UserService} from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [UserService]
 })
-export class AppComponent {
-  title = 'src';
+export class AppComponent implements OnInit {
+  constructor(private userService: UserService){}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+
+  public getUsers(): void{
+    this.userService.getUsers().subscribe((res) => {
+      this.userService.users = res as User[];
+    });
+  }
+
+  public postUser(form: NgForm): void{
+    if (form.value._id){
+      this.userService.postUser(form.value).subscribe((res) => {
+        this.getUsers();
+        this.userService.selectedUser = new User();
+      });
+    } else{
+      this.userService.putUsers(form.value).subscribe((res) => {
+        this.getUsers();
+        this.userService.selectedUser = new User();
+      });
+    }
+  }
+
+  public editUser(user: User): void {
+    this.userService.selectedUser = user;
+  }
+
+  public deleteUser(_id: string): void {
+    this.userService.deleteUser(_id).subscribe((res) => {
+      this.getUsers();
+      this.userService.selectedUser = new User();
+    });
+  }
 }
